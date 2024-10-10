b18a7954da7dd45c9ccf1c2511fd5258a4a99cb6
stoyanivanov <stoyanivanov03@gmail.com>
2024-10-10 11:55:42 +0300

    Meal Logic

---
+++ src/controller/UserController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import service.UserService;
+
+import java.util.Scanner;
+
+public class UserController {
+    private final  UserService userService = new UserService();
+    private final Scanner sc = new Scanner(System.in);
+
+}
